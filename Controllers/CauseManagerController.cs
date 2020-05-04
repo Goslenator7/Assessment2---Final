37eb65db7b3b2719244a82e369a0f5a8643566bd
Ryan <Ryan@DESKTOP-EJCIBQH>
2020-05-04 18:19:29 +0100

    uygguily

---
+++ Controllers/CauseManagerController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assessment2___Final.Data;
+using Assessment2___Final.Models;
+
+namespace Assessment2___Final.Controllers
+{
+    public class CauseManagerController : Controller
+    {
+        private Assessment2___FinalDBContext db = new Assessment2___FinalDBContext();
+
+        // GET: CauseManager
+        public ActionResult Index()
+        {
+            var causes = db.Causes.Include(c => c.User);
+            return View(causes.ToList());
+        }
+
+        // GET: CauseManager/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cause cause = db.Causes.Find(id);
+            if (cause == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cause);
+        }
+
+        // GET: CauseManager/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username");
+            return View();
+        }
+
+        // POST: CauseManager/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CauseID,Title,Category,Description,Image,UserID")] Cause cause)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Causes.Add(cause);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", cause.UserID);
+            return View(cause);
+        }
+
+        // GET: CauseManager/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cause cause = db.Causes.Find(id);
+            if (cause == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", cause.UserID);
+            return View(cause);
+        }
+
+        // POST: CauseManager/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CauseID,Title,Category,Description,Image,UserID")] Cause cause)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cause).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", cause.UserID);
+            return View(cause);
+        }
+
+        // GET: CauseManager/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cause cause = db.Causes.Find(id);
+            if (cause == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cause);
+        }
+
+        // POST: CauseManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cause cause = db.Causes.Find(id);
+            db.Causes.Remove(cause);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
